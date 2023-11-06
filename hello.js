be6baa9f2e4ae227a7711fde1e2d210ab69b64da
rodrigoflstk <jrodrigo0592@gmail.com>
2023-11-06 09:00:42 -0300

    hello.js

---
+++ hello.js
@@ -0,0 +1,8 @@
+var http = require("http")
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {'content-type': 'text/plain'})
+  response.end('Hello world!')
+}).listen(8124)
+
+console.log("Server is listening on port 8124")
